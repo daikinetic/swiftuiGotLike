1e1906220f0930938a3add4aeb038cfd943850fd
daikinetic <dddaiki.geek@gmail.com>
2023-05-30 17:37:42 +0900

    Initial Commit

---
+++ swiftuiGotLikeView/swiftuiGotLikeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiGotLikeViewApp.swift
+//  swiftuiGotLikeView
+//
+//  Created by Daiki Takano on 2023/05/30.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiGotLikeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
