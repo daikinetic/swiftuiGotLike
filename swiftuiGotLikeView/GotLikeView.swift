30686785a3435e28f1d03bc3a80c3c46a95a8159
daikinetic <dddaiki.geek@gmail.com>
2023-06-09 21:54:14 +0900

    Overlayの位置がおかしかったので修正しました

--- swiftuiGotLikeView/GotLikeView.swift
+++ swiftuiGotLikeView/GotLikeView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftUIGestureVelocity
+import SwiftUISnapDraggingModifier
 
 struct GotLikeView: View {
     
@@ -109,31 +110,8 @@ struct GridCell: View {
                         .foregroundColor(.white)
                         .padding(.top, 10)
                         .padding(.trailing, 9)
-                        .overlay(alignment: .topTrailing) {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 20)
-                                    .frame(width: 155.5, height: 220)
-                                    .foregroundColor(.white.opacity(0))
-                                    .background(
-                                        LinearGradient(
-                                            gradient: Gradient(colors: [Color(0x000000).opacity(0.5),Color(0x000000).opacity(0)]),
-                                            startPoint: .init(x: 1, y: 0),    // start地点
-                                            endPoint: .init(x: 0, y: 1)     // end地点
-                                        ))
-                                    .cornerRadius(20, style: .continuous)
-                                
-                                Image("nope")
-                                    .renderingMode(.template)
-                                    .resizable()
-                                    .aspectRatio(contentMode:.fit)
-                                    .frame(width: 36)
-                                    .foregroundColor(Color(0x96AAAA))
-                                    .offset(x: 30, y: -85)
-                                    .rotationEffect(Angle(degrees: 10))
-                            }
-                            .opacity(isRotate ? getOverlayAmount() : 0)
-                        }
                 }
+                
                 Text(message)
                     .frame(width:130)
                     .padding(.vertical, 10)
@@ -147,6 +125,30 @@ struct GridCell: View {
                     .cornerRadius(16)
                     .padding(.bottom, 8)
                     .font(.system(size: 14, weight: .bold))
+            }
+            .overlay(alignment: .topTrailing) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 155.5, height: 220)
+                        .foregroundColor(.white.opacity(0))
+                        .background(
+                            LinearGradient(
+                                gradient: Gradient(colors: [Color(0x000000).opacity(0.5),Color(0x000000).opacity(0)]),
+                                startPoint: .init(x: 1, y: 0),    // start地点
+                                endPoint: .init(x: 0, y: 1)     // end地点
+                            ))
+                        .cornerRadius(20, style: .continuous)
+                    
+                    Image("nope")
+                        .renderingMode(.template)
+                        .resizable()
+                        .aspectRatio(contentMode:.fit)
+                        .frame(width: 36)
+                        .foregroundColor(Color(0x96AAAA))
+                        .offset(x: 30, y: -85)
+                        .rotationEffect(Angle(degrees: 10))
+                }
+                .opacity(isRotate ? getOverlayAmount() : 0)
             }
             .padding(.bottom, 8)
             
